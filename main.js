feff681cd0d2950742a48bb7ae1b6fcd807ceae3
Dinhuty <93468153+dinhuty@users.noreply.github.com>
2022-03-08 16:30:11 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// var okE= document.getElementsByClassName('ok')
+// console.log(okE)
+var ulE = document.getElementById('tp1')
+var diadiemE= document.getElementById('ok')
+
+    ulE.onclick =function (e) {
+        diadiemE.classList.toggle('diadiem2')
+    }
+
+// okE.onclick= function (e) {
+//     // alert('ok')
+//     console.log(e)
+//
+// }
+// okE.onclick= function (){
+//     console.log('viec 1')
+//
+//     console.log('viec 1')
+//
+// }
+var btn= document.getElementById('btn')
+btn.onclick= function (){
+    console.log('viec 1')
+
+    console.log('viec 1')
+
+}
